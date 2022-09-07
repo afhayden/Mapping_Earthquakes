ab43aafa20a49c33438251568bb9fff2562aa540
afhayden <100769036+afhayden@users.noreply.github.com>
2022-09-07 18:04:29 -0400

    initialize folders and files

---
+++ Mapping_GeoJSON_Polygons/static/js/airports.js
@@ -0,0 +1,58 @@
+var airports = [
+  {
+    location: [33.9416, -118.4085],
+    city: "Los Angeles International Airport",
+    acronym: "LAX",
+    state: "CA",
+    trip_num: 1
+  },
+  {
+    location: [37.6213, -122.3790],
+    city: "San Francisco International Airport",
+    acronym: "SFO",
+    state: "CA",
+    trip_num: 1
+  },
+  {
+    location: [40.7899, -111.9791],
+    city: "Salt Lake City International Airport",
+    acronym: "SLC",
+    state: "UT",
+    trip_num: 1
+  },
+  {
+    location: [47.4502, -122.3088],
+    city: "Seattle-Tacoma International Airport",
+    acronym: "SEA",
+    state: "WA",
+    trip_num: 1
+  },
+  {
+    location: [37.6213, -122.3790],
+    city: "San Francisco International Airport",
+    acronym: "SFO",
+    state: "CA",
+    trip_num: 1
+  },
+  {
+    location: [30.1975, -97.6664],
+    city: "Austin-Bergstrom International Airport",
+    acronym: "AUS",
+    state: "TX",
+    trip_num: 3
+  },
+  {
+    location: [43.6777, -79.6248],
+    city: "Toronto Pearson International Airport",
+    acronym: "YYZ",
+    state: "CAN",
+    trip_num: 4
+  },
+  {
+    location: [40.6413, -73.7781],
+    city: "John F. Kennedy International Airport",
+    acronym: "JFK",
+    state: "NY",
+    trip_num: 2
+  }
+];
