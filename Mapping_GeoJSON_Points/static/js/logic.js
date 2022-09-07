b57683eb071468afbb3b27aefb0e7106619a6262
afhayden <100769036+afhayden@users.noreply.github.com>
2022-09-07 16:50:11 -0400

    adding JSON file

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -2,76 +2,62 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 4);
-
-// Coordinates for each point to be used in the line.
-let line = [];
-
-airports.forEach(x => line.push(x.location));
-console.log(line);
-
-// Create a polyline using the line coordinates and make the line red.
-L.polyline(line, {
-  color: "blue",
-  weight: 4,
-  opacity: 0.5,
-  dashArray: "7"
-}).addTo(map);
-
-// Create the map object with a center and zoom level.
-// let map = L.map("mapid", {
-//     center: [
-//       40.7, -94.5
-//     ],
-//     zoom: 14
-//   });
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
-//  Add a marker to the map for Los Angeles, California.
-//let marker = L.marker([34.0522, -118.2437]).addTo(map);
-
-// L.circle([34.0522, -118.2437], {
-//   radius: 100
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+// Grabbing our GeoJSON data.
+// L.geoJSON(sanFranAirport, {
+//    // We turn each feature into a marker on the map.
+//    pointToLayer: function(feature, latlng) {
+//     console.log(feature);
+//     return L.marker(latlng)
+//     .bindPopup("<h2>" + feature.properties.name + "</h2><hr><h3>" + feature.properties.city + ", " + feature.properties.country + "<h3>");
+//   }
 // }).addTo(map);
 
-// L.circle([34.0522, -118.2437], {
-//   radius: 300,
-//   fillOpacity: 0.75,
-//     color: "black",
-//     fillColor: "pink"
-// }).addTo(map);
+L.geoJSON(sanFranAirport, {
+  // We turn each feature into a marker on the map.
+  onEachFeature: function(feature, layer) {
+   console.log(layer);
+   layer.bindPopup();
+ }
+}).addTo(map);
 
-// L.circleMarker([34.0522, -118.2437], {
-//   radius: 300,
-//   color: "black",
-//   fillColor: '#ffffa1'
-// }).addTo(map);
+L.geoJSON(sanFranAirport, {
+  // We turn each feature into a marker on the map.
+  onEachFeature: function(feature, layer) {
+   console.log(layer);
+   layer.bindPopup("<h2>Airport code: " + feature.properties.faa + "</h2><hr><h3>Airport name: " + feature.properties.name + ", " + feature.properties.country + "<h3>");
+ }
+}).addTo(map);
+
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
-// An array containing each city's location, state, and population.
-// Get data from cities.js
-// let cityData = cities;
-// console.log(cities);
-// const numberFormatter = Intl.NumberFormat('en-US');
-
-// cityData.forEach(city => {
-//   console.log(city);
-//   L.circleMarker(city.location, {
-//     radius: city.population/200000,
-//     weight: 4,
-//     fillOpacity: 0.25,
-//     color: '#ff9933',
-//     fillColor: '#ff9933'
-//   }).addTo(map)
-//   .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + numberFormatter.format(city.population) + "</h3>")
-//   .addTo(map);
-// });
 
